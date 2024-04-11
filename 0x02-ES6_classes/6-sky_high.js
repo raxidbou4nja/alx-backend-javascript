49e1e32aaf20d0f27141bb46babea4645e7f1ca4
raxidbou4nja <rachid.boughnja@gmail.com>
2024-04-11 19:24:32 +0000

    Fix floors setter in SkyHighBuilding class

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,15 +3,15 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._floors = floors;
+    this.floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
-  get sqft() {
-    return this._sqft;
+  set floors(value) {
+    this._floors = value;
   }
 
   evacuationWarningMessage() {
