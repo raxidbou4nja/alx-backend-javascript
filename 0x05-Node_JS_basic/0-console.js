0353b27212527a959b437ddb8d41f2aad7b16139
raxidbou4nja <125773922+raxidbou4nja@users.noreply.github.com>
2024-06-05 00:49:34 +0100

    Create 0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,13 @@
+/**
+ * Prints a message to the console.
+ * @param {string} message - The message to be printed.
+ * @returns {void}
+ */
+function printMessage(message) {
+    console.log(message);
+}
+
+/**
+ * Exports the printMessage function.
+ */
+module.exports = printMessage;
