4be1dd9ff10a2daeed5fcaae8a2ab0c77495b216
raxidbou4nja <125773922+raxidbou4nja@users.noreply.github.com>
2024-06-05 01:13:07 +0100

    Update index.js

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,12 +1,20 @@
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
-
+/**
+ * Map routes to corresponding controllers.
+ * @param {Object} app - The Express application object.
+ */
 const mapRoutes = (app) => {
+  // Map homepage route to AppController's method
   app.get('/', AppController.getHomepage);
+  
+  // Map /students route to StudentsController's method for retrieving all students
   app.get('/students', StudentsController.getAllStudents);
+  
+  // Map /students/:major route to StudentsController's method for retrieving students by major
   app.get('/students/:major', StudentsController.getAllStudentsByMajor);
 };
 
+// Export the mapRoutes function
 export default mapRoutes;
-module.exports = mapRoutes;
