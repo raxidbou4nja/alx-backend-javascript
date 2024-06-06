b694c467b5113235114e54ec67a9e18cf68e426b
raxidbou4nja <rachid.boughnja@gmail.com>
2024-06-06 17:43:17 +0100

    chore: Refactor sendPaymentRequestToApi unit test and update assertion for total amount

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,15 +1,29 @@
 const sinon = require('sinon');
 const Utils = require('./utils');
+const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./5-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('should call Utils.calculateNumber with the correct arguments', () => {
-    const spy = sinon.spy(Utils, 'calculateNumber');
+  let stub;
+
+  beforeEach(() => {
+    stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+  });
+
+  afterEach(() => {
+    stub.restore();
+  });
+
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const spy = sinon.spy(console, 'log');
 
     sendPaymentRequestToApi(100, 20);
 
-    expect(spy.calledOnce).to.be.true;
-    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+    sinon.assert.calledOnce(stub);
+    sinon.assert.calledWith(stub, 'SUM', 100, 20);
+
+    sinon.assert.calledOnce(spy);
+    sinon.assert.calledWith(spy, 'The total is: 10');
 
     spy.restore();
   });
