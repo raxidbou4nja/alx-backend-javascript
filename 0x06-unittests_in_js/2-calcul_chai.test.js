27ccb18e491b4f481e5de87e6615dd3efa76493a
raxidbou4nja <rachid.boughnja@gmail.com>
2024-06-06 16:58:46 +0100

    chore: Update chai dependency to version 4.3.7

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const calculateNumber = require('./2-calcul');
+const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', () => {
     it('should return 4', () => {
