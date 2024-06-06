5d4adc0e7482a554c6b6816b62153ed6ec7465c1
raxidbou4nja <rachid.boughnja@gmail.com>
2024-06-06 17:29:39 +0100

    chore: Refactor sendPaymentRequestToApi unit test

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,21 +1,29 @@
 const sinon = require('sinon');
-const { expect } = require('chai');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('should call Utils.calculateNumber with the correct arguments', () => {
-    const spy = sinon.spy(Utils, 'calculateNumber');
-    const consoleSpy = sinon.spy(console, 'log');
+  let stub;
+
+  beforeEach(() => {
+    stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+  });
+
+  afterEach(() => {
+    stub.restore();
+  });
+
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const spy = sinon.spy(console, 'log');
 
     sendPaymentRequestToApi(100, 20);
 
-    expect(spy.calledOnce).to.be.true;
-    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+    sinon.assert.calledOnce(stub);
+    sinon.assert.calledWith(stub, 'SUM', 100, 20);
 
-    expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+    sinon.assert.calledOnce(spy);
+    sinon.assert.calledWith(spy, 'The total is: 120');
 
     spy.restore();
-    consoleSpy.restore();
   });
 });
